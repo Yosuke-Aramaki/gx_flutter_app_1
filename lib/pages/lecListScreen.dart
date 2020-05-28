b6daff1b2a681fdc6892b5c8730d85e48cc2a3b4
Yosuke-Aramaki <gnx.vw903@gmail.com>
2020-05-29 08:43:19 +0900

    changed design in lecture list screen

--- lib/pages/lecListScreen.dart
+++ lib/pages/lecListScreen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:gx_app_1/api/firestoreState.dart';
 
 class LectureListScreen extends StatefulWidget {
   @override 
@@ -10,6 +11,16 @@ class LectureListScreen extends StatefulWidget {
 
 class _LectureListScreen extends State<LectureListScreen> {
   var lectureInformation = new Map();
+  var arguments = new Map();
+  Map<String, dynamic> lectureSummaryInfo;
+
+  void getLectureSummaryInfo(String _department, String _lecture) async {
+    await FirestoreState.getLectureSummaryInfo(lectureInformation['university'], _department, _lecture).then((value) {
+      setState(() {
+        lectureSummaryInfo = value;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -81,61 +92,96 @@ class _LectureListScreen extends State<LectureListScreen> {
   Widget _buildAllLecture(Map lectureInformation) {
     return ListView.builder(
       itemCount: lectureInformation['departmentList'].length,
-      itemBuilder: (BuildContext context, int index) {
+      itemBuilder: (BuildContext context, int index1) {
         return Container(
-          padding: const EdgeInsets.only(top: 15.0, bottom: 15.0, left: 8.0),
+          padding: const EdgeInsets.only(top: 15.0, left: 8.0, right: 8.0, bottom: 20.0),
           decoration: BoxDecoration(
             border: Border(top: BorderSide(color: Colors.grey)),
           ),
           child: Column(
             children: <Widget>[
-              _buildDepartmentName(lectureInformation['departmentList'][index]),
+              _buildDepartmentName(lectureInformation['departmentList'][index1]),
               Column(children: <Widget>[
                 StreamBuilder(
-                  stream: Firestore.instance.collection('univ_list').document(lectureInformation['university']).collection('dep_list').document(lectureInformation['departmentList'][index]).collection('lec_list')
+                  stream: Firestore.instance.collection('univ_list').document(lectureInformation['university']).collection('dep_list').document(lectureInformation['departmentList'][index1]).collection('lec_list')
                     .snapshots(),
                   builder: (context, snapshot) {
                     if (!snapshot.hasData) return const Text("Loading...");
                     return ListView.builder(
                       shrinkWrap: true,
                       itemCount: snapshot.data.documents.length,
-                      itemBuilder: (context, index) {
-                        final record = LectureSummary.fromSnapshot(snapshot.data.documents[index]);
-                        return SizedBox(
-                          child: Container(
-                            margin: EdgeInsets.symmetric(vertical: 10.0),
-                            padding: EdgeInsets.all(15.0),
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.grey[600]),
-                              borderRadius: BorderRadius.circular(10),
-                              color: const Color(0xffFAFAFA),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black26,
-                                  spreadRadius: 1.0,
-                                  blurRadius: 5.0,
-                                  offset: Offset(5, 5),
-                                ),
-                              ],
-                            ),
-                            child: Column(
-                              children: <Widget>[
-                                Container(
-                                  padding: EdgeInsets.only(bottom: 10.0),
-                                  alignment: Alignment.centerLeft,
-                                  child: Text(
-                                    record.name,
-                                    style: TextStyle(fontSize: 16),
+                      itemBuilder: (context, index2) {
+                        final record = LectureSummary.fromSnapshot(snapshot.data.documents[index2]);
+                        return GestureDetector(
+                          onTap: () async {
+                            getLectureSummaryInfo(lectureInformation['departmentList'][index1], record.name);
+                            arguments ={'university': lectureInformation['university'], 'department': lectureInformation['departmentList'][index1], 'lecture': record.name, 'lectureSummary': lectureSummaryInfo};
+                            Navigator.pushNamed(
+                              context,
+                              '/LectureInformationScreen',
+                              arguments: arguments,
+                            );
+                          },
+                          child: SizedBox(
+                            child: Container(
+                              margin: EdgeInsets.symmetric(vertical: 10.0),
+                              padding: EdgeInsets.all(15.0),
+                              decoration: BoxDecoration(
+                                border: Border.all(color: Colors.grey[600]),
+                                borderRadius: BorderRadius.circular(10),
+                                color: const Color(0xffFAFAFA),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black26,
+                                    spreadRadius: 1.0,
+                                    blurRadius: 5.0,
+                                    offset: Offset(5, 5),
                                   ),
-                                ),
-                                Row(
-                                  children: <Widget>[
-                                    _buildStarRate(record.qualityAvg.round()),
-                                    _buildStarRate(record.difficultyAvg.round()),
-                                    Text(record.quantity.toString())
-                                  ],
-                                ),
-                              ],
+                                ],
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                        record.name,
+                                        style: TextStyle(fontSize: 16),
+                                      ),
+                                      SizedBox(height: 10.0),
+                                      Row(
+                                        children: <Widget>[
+                                          Text(
+                                            '質',
+                                            style: TextStyle(fontSize: 12),
+                                          ),
+                                          SizedBox(width: 4.0),
+                                          _buildStarRate(record.qualityAvg.round()),
+                                          SizedBox(width: 14.0),
+                                          Text(
+                                            '難易度',
+                                            style: TextStyle(fontSize: 12),
+                                          ),
+                                          SizedBox(width: 4.0),
+                                          _buildStarRate(record.difficultyAvg.round()),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                  Column(
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.keyboard_arrow_right ,
+                                      ),
+                                      Text(
+                                        record.quantity.toString() + '件の評価',
+                                        style: TextStyle(fontSize: 10),
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         );
@@ -151,6 +197,17 @@ class _LectureListScreen extends State<LectureListScreen> {
     );
   }
 
+  Widget _buildStarRate(int value) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: List.generate(5, (index) {
+        return Icon(
+          index < value ? Icons.star : Icons.star_border,
+          size: 18.0,
+        );
+      }),
+    );
+  }
 
   Widget _buildBody(BuildContext context, Map lectureInformation) {
     return StreamBuilder<QuerySnapshot>(
@@ -169,17 +226,6 @@ class _LectureListScreen extends State<LectureListScreen> {
     );
   }
   
-  Widget _buildStarRate(int value) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: List.generate(5, (index) {
-        return Icon(
-          index < value ? Icons.star : Icons.star_border,
-        );
-      }),
-    );
-  }
-
   Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
     final record = LectureSummary.fromSnapshot(data);
     return SizedBox(
